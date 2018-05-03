945fac95f76fa31596fa5b0a7e5681c78b7ae105
Adriaan van Rossum <git@adriaan.io>
2018-05-03 23:16:31 +0100

    Add logs and remove hostname

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const http = require('http');
 const SimpleBase = require('simple-base');
 
-const hostname = '127.0.0.1';
 const port = process.env.PORT || '3000';
 
 const server = http.createServer((req, res) => {
+
+  console.log('req.headers.host', req.headers.host);
+
   const hostnameWithPort = req.headers.host.split('.').slice(-2).join('.');
   const hostname = hostnameWithPort.split(':')[0];
+
+  console.log('hostname', hostname);
 
   if (req.headers.host.split('.').length === 2) {
     return res.end('Homepage of dnsforwarding.com');
@@ -15,12 +19,14 @@ const server = http.createServer((req, res) => {
   const subdomainCode = req.headers.host.split('.').slice(0, -2).join('');
   const location = SimpleBase.decode(subdomainCode, 36);
   if (location.indexOf('http') === 0) {
+    console.log('redirect to:', location);
     res.writeHead(302, { 'Location': location });
     return res.end();
   }
+  console.error('something is wrong with decoding:', location);
   res.end('Something is wrong with your subdomain');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
