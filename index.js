98f1dff681a25a937239e7dc5e96908e34d3835a
Adriaan van Rossum <git@adriaan.io>
2018-05-03 23:12:08 +0100

    Add more feedback

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ const port = process.env.PORT || '3000';
 const server = http.createServer((req, res) => {
   const hostnameWithPort = req.headers.host.split('.').slice(-2).join('.');
   const hostname = hostnameWithPort.split(':')[0];
+
+  if (req.headers.host.split('.').length === 2) {
+    return res.end('Homepage of dnsforwarding.com');
+  }
+
   const subdomainCode = req.headers.host.split('.').slice(0, -2).join('');
   const location = SimpleBase.decode(subdomainCode, 36);
-  res.writeHead(302, { 'Location': location });
-  res.end();
+  if (location.indexOf('http') === 0) {
+    res.writeHead(302, { 'Location': location });
+    return res.end();
+  }
+  res.end('Something is wrong with your subdomain');
 });
 
 server.listen(port, hostname, () => {
