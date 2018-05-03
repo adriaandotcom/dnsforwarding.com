4c1421b0038798bf875e7a8ddb1a24b0724d3d67
Adriaan van Rossum <git@adriaan.io>
2018-05-04 00:21:02 +0100

    Log all headers

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const server = http.createServer((req, res) => {
   console.log('req.headers.host', req.headers.host);
   console.log('req.headers.x-upstream', req.headers['X-Upstream']);
 
+  console.log(JSON.stringify(req.headers));
+
   // const hostname = req.headers.host.split(':')[0];
   const hostnameWithPort = req.headers.host.split('.').slice(-2).join('.');
   const hostname = hostnameWithPort.split(':')[0];
