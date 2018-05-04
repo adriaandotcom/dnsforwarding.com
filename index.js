b1b23da67deef8f477e7f4e6d30398f180843423
Adriaan van Rossum <git@adriaan.io>
2018-05-04 01:21:12 +0100

    Use dns for cname

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 const http = require('http');
+const dns = require('dns');
 const SimpleBase = require('simple-base');
 
 const port = process.env.PORT || '3000';
 
-const server = http.createServer((req, res) => {
+function getSubdomains(url) {
+  const hostname = url.split(':')[0];
+  const subdomainCode = hostname.split('.').slice(0, -2).join('');
+  const location = SimpleBase.decode(subdomainCode, 36);
+  if (location.indexOf('http') === 0) return location;
+  return false;
+}
+
+const server = http.createServer(async (req, res) => {
   try {
     const host = req.headers.host;
     if (!host) return res.end('Something is wrong at our end, mail us. #nohost');
-    const hostname = host.split(':')[0];
 
-    console.log('hostname', hostname);
+    if (host.split('.').length === 2) return res.end('Homepage of dnsforwarding.com');
 
-    if (hostname.split('.').length === 2) {
-      return res.end('Homepage of dnsforwarding.com');
-    }
-
-    const subdomainCode = hostname.split('.').slice(0, -2).join('');
-    const location = SimpleBase.decode(subdomainCode, 36);
-    if (location.indexOf('http') === 0) {
-      console.log('redirect to:', location);
-      res.writeHead(302, { 'Location': location });
+    // Find url based on hostname
+    const hostLocation = getSubdomainRedirect(host);
+    if (hostLocation) {
+      res.writeHead(302, { 'Location': hostLocation });
       return res.end();
     }
-    console.error('something is wrong with decoding:', location);
-    res.end('Something is wrong with your subdomain');
+
+    // Find url based on cname of hostname
+    const cnames = await dns.resolveCname(host);
+    if (cnames && cnames[0]) {
+      const cnameLocation = getSubdomainRedirect(cnames[0]);
+      if (cnameLocation) {
+        res.writeHead(302, { 'Location': cnameLocation });
+        return res.end();
+      }
+    }
+
+    return res.end('No redirect found in this url. #notfound');
   } catch (e) {
     console.error(e);
     res.end('Something is wrong at our end, mail us. #catch');
